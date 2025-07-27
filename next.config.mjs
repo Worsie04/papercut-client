f3b99a2c1e2f3a30da7982f94f6a378cbd307258
Worsie04 <developer@worsie.com>
2025-07-27 00:21:34 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,11 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  // Azure üçün standalone output-u deaktiv edin
-  // output: 'standalone',
+  output: 'standalone',
   experimental: {
     workerThreads: false,
     cpus: 1,
   },
-  // Azure üçün əlavə ayarlar
-  generateEtags: false,
-  poweredByHeader: false,
-  compress: true,
   async rewrites() {
     return [
       {
@@ -29,6 +24,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Origin', value: 'https://papercut-backend-1.onrender.com' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+
           // Cache control başlıqları
           { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
           { key: 'Pragma', value: 'no-cache' },
@@ -38,4 +34,5 @@ const nextConfig = {
     ];
   },
 };
+
 export default nextConfig;
